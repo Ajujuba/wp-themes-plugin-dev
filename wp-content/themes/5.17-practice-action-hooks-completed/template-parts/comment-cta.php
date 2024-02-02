70f11c35100d30160ca6b82d0d4ee48317409684
Ana Alves <60891489+Ajujuba@users.noreply.github.com>
2024-02-02 09:04:00 +0000

    feat: add new theme with action hooks
    - Studyng about action hooks, using some exemples in functions.php how pre_get_comments or admin_enqueue_scripts

---
+++ wp-content/themes/5.17-practice-action-hooks-completed/template-parts/comment-cta.php
@@ -0,0 +1,4 @@
+<!-- the hook pre_get_comments will get this part , the code is in funtions.php -->
+<div class="comment-cta">
+  <h2><?php esc_html_e( 'Join in the Discussion!!!', 'wphooks' ); ?></h2>
+</div>
